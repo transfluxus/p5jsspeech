da01f8912205a9d10a8239697f39b2342b474968
Ramin <ramin.soleymani@ixds.com>
2015-07-18 13:53:26 +0200

    cleaned index and some action

--- action.js
+++ action.js
@@ -1,12 +1,34 @@
-function setup() {
-  createCanvas(640, 480);
+var CIRCLE = 0;
+var SQUARE = 1;
+var LINE = 2;
+
+//var position = createVector(-1,-1);
+
+function drawObject(objectIndex) {
+	if( objectIndex == CIRCLE) {
+		var position = randomPosition();
+		var clr = color(255,0,0);
+		var sz = random(30,60); 
+		drawCircle(position, clr,sz);
+	} else if( objectIndex == SQUARE) {
+
+	} else if( objectIndex == LINE) {
+
+	}
 }
 
-function draw() {
-  if (mouseIsPressed) {
-    fill(0);
-  } else {
-    fill(255);
-  }
-  ellipse(mouseX, mouseY, 80, 80);
+function randomPosition() {
+	return createVector(random(width), random(height));
+}
+
+function drawCircle(position, color, size) {
+	noStroke();
+	fill(color);
+	ellipse(position.x,position.y,size,size);
+}
+
+function drawRectangle(position,color,size) {
+	noStroke();
+	fill(color);
+	rect(position.x,position.y,size,size);	
 }
