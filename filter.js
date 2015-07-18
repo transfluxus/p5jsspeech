340339db2f8af4952181ce2ddded7ffd1acbb700
edapx <lastexxit@gmail.com>
2015-07-18 15:20:12 +0200

    add instruction

--- filter.js
+++ filter.js
@@ -2,25 +2,42 @@
 var Filter = function (words) {
     this.available_objects = words.objects;
     this.available_colors = words.colors;
-    this.available_sizes = {};
+    this.instruction = '';
     this.detected_object = null;
-    this.detected_color = null;
+    this.detected_object = null;
+    this.detected_size = null;
 
     this.grab_object = function grab(microphone){
         for(var w in this.available_objects) {
             if (this.available_objects.hasOwnProperty(microphone)) {
-                this.detected_object = w;
+                this.detected_object = microphone;
             }
         }
     };
 
     this.grab_size = function grab_size(microphone){
+        var number = parseInt(microphone);
+        if(!isNaN(number)) {
+            console.log("number detected "+number);
+            this.detected_size = number;
+        }
     };
 
     this.grab_color = function grab_color(microphone){
+        for(var w in this.available_colors) {
+            if (this.available_colors.hasOwnProperty(microphone)) {
+                this.detected_colors = microphone;
+            }
+        }
     };
 
-    this.instruction = { "object":0, "size":1, "color":2 };
+    this.instruction = {
+        "object": this.detected_object,
+        "size": this.detected_size,
+        "color": this.detected_color
+    };
+
+    return this;
 };
 
 
