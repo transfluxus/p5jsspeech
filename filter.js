a3defacf62bf168efdf90a166f68090d4cb792d8
edapx <lastexxit@gmail.com>
2015-07-18 15:41:04 +0200

    fix the filter

--- filter.js
+++ filter.js
@@ -1,4 +1,5 @@
 "use strict";
+
 var Filter = function (words) {
     this.available_objects = words.objects;
     this.available_colors = words.colors;
@@ -6,38 +7,37 @@ var Filter = function (words) {
     this.detected_object = null;
     this.detected_object = null;
     this.detected_size = null;
+};
 
-    this.grab_object = function grab(microphone){
-        for(var w in this.available_objects) {
-            if (this.available_objects.hasOwnProperty(microphone)) {
-                this.detected_object = microphone;
-            }
+Filter.prototype.grab_object = function(microphone){
+    for(var w in this.available_objects) {
+        if (this.available_objects.hasOwnProperty(microphone)) {
+            this.detected_object = microphone;
         }
-    };
+    }
+};
 
-    this.grab_size = function grab_size(microphone){
-        var number = parseInt(microphone);
-        if(!isNaN(number)) {
-            console.log("number detected "+number);
-            this.detected_size = number;
+Filter.prototype.grab_size = function(microphone){
+    var number = parseInt(microphone);
+    if(!isNaN(number)) {
+        console.log("number detected "+number);
+        this.detected_size = number;
+    }
+};
+
+Filter.prototype.grab_color = function(microphone){
+    for(var w in this.available_colors) {
+        if (this.available_colors.hasOwnProperty(microphone)) {
+            this.detected_colors = microphone;
         }
-    };
+    }
+};
 
-    this.grab_color = function grab_color(microphone){
-        for(var w in this.available_colors) {
-            if (this.available_colors.hasOwnProperty(microphone)) {
-                this.detected_colors = microphone;
-            }
-        }
-    };
-
-    this.instruction = {
-        "object": this.detected_object,
-        "size": this.detected_size,
-        "color": this.detected_color
-    };
-
-    return this;
+Filter.prototype.instruction = function(){
+    return {"blu" : "bla"};
+    // { "object": this.detected_object,
+    // "size": this.detected_size,
+    // "color": this.detected_color }
 };
 
 
